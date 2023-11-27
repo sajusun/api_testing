54e3168ade35858352cd590fbf9a40f46f4ff075
sajusun <sajuislam266@gmail.com>
2023-11-27 15:34:51 +0600

    with loading cercularbar

--- lib/widget/product_card.dart
+++ lib/widget/product_card.dart
@@ -11,15 +11,15 @@ class ProductCard extends StatefulWidget {
 }
 
 class _ProductCardState extends State<ProductCard> {
+  List<Datum> datum = [];
 
-List<Datum> datum=[];
   getData() async {
-    ProductData data= await AllProductsApi.getFromApi();
+    ProductData data = await AllProductsApi.getFromApi();
     print(data.data.data);
     datum.addAll(data.data.data);
-    setState(() {
-    });
+    setState(() {});
   }
+
   @override
   void initState() {
     super.initState();
@@ -29,40 +29,56 @@ List<Datum> datum=[];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
+      body: isNotError(),
+    );
+  }
+  // check api data is succesfully fetch or display a circular bar
+  isNotError(){
+    if(datum.isNotEmpty){
+      return Padding(
         padding: const EdgeInsets.only(top: 48.0),
         child: SizedBox(
           height: 350,
           width: MediaQuery.of(context).size.width,
           child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: datum.length,
-              itemBuilder: (context,item){
-          return  SizedBox(
-            width: 300,
-            child: Card(
-              clipBehavior: Clip.hardEdge,
-                child: Column(
-                  children: [
-                    image.Image.network(datum[item].image.medium,height: 230,fit: BoxFit.cover,),
-                    Text(datum[item].name,style: TextStyle(color: Colors.black,height: 3)),
-                    SizedBox(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text("Regular : ${datum[item].regularPrice}"),
-                          Text("Offer : ${datum[item].discountedPrice}"),
-                        ],
-                      ),
+              scrollDirection: Axis.horizontal,
+              itemCount: datum.length,
+              itemBuilder: (context, item) {
+                return SizedBox(
+                  width: 300,
+                  child: Card(
+                    clipBehavior: Clip.hardEdge,
+                    child: Column(
+                      children: [
+                        image.Image.network(
+                          datum[item].image.medium,
+                          height: 230,
+                          fit: BoxFit.cover,
+                        ),
+                        Text(datum[item].name,
+                            style: TextStyle(color: Colors.black,fontSize: 16, height: 3)),
+                        SizedBox(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text("Regular : ${datum[item].regularPrice}"),
+                              Text("Offer : ${datum[item].discountedPrice}"),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          "Discount : ${datum[item].discount}",
+                          style: TextStyle(height: 3),
+                        ),
+                      ],
                     ),
-                    Text("Discount : ${datum[item].discount}",style: TextStyle(height: 3),),
-                  ],
-                ),
-              ),
-          );
-          }),
+                  ),
+                );
+              }),
         ),
-      ),
-    );
+      );
+    }else{
+      return Center(child: CircularProgressIndicator());
+    }
   }
 }
